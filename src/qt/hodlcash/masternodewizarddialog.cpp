91dcbac500b84719a7ef171a59f0834ec2a45e29
MichaelHDesigns <43361618+MichaelHDesigns@users.noreply.github.com>
2021-05-22 02:55:32 -0400

    Update masternodewizarddialog.cpp

--- src/qt/hodlcash/masternodewizarddialog.cpp
+++ src/qt/hodlcash/masternodewizarddialog.cpp
@@ -67,9 +67,9 @@ MasterNodeWizardDialog::MasterNodeWizardDialog(WalletModel *model, QWidget *pare
     ui->lineEditPort->setValidator(new QIntValidator(0, 9999999, ui->lineEditPort));
     if(walletModel->isTestNetwork()){
         ui->lineEditPort->setEnabled(false);
-        ui->lineEditPort->setText("996105");
+        ui->lineEditPort->setText("51474");
     } else {
-        ui->lineEditPort->setText("610599");
+        ui->lineEditPort->setText("6744");
     }
 
     // Confirm icons
@@ -282,7 +282,7 @@ bool MasterNodeWizardDialog::createMN(){
                 if (lineCopy.size() == 0) {
                     lineCopy = "# Masternode config file\n"
                                "# Format: alias IP:port masternodeprivkey collateral_output_txid collateral_output_index\n"
-                               "# Example: mn1 127.0.0.2:610599 93HaYBVUCYjEMeeH1Y4sBGLALQZE1Yc1K64xiqgX37tGBDQL8Xg 2bcd3c84c84f87eaa86e4e56834c92927a07f9e18718810b92e0d0324456a67c 0"
+                               "# Example: mn1 127.0.0.2:51474 93HaYBVUCYjEMeeH1Y4sBGLALQZE1Yc1K64xiqgX37tGBDQL8Xg 2bcd3c84c84f87eaa86e4e56834c92927a07f9e18718810b92e0d0324456a67c 0"
                                "#";
                 }
                 lineCopy += "\n";
